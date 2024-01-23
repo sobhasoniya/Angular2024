b67493f868bb2d7fd796648a246590afe5c6ffa7
DeepanRT <deepandeepz24@gmail.com>
2024-01-23 17:54:33 +0530

    Pharmacist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { NgxPaginationModule } from 'ngx-pagination';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { ToastrModule } from 'ngx-toastr';
+import { HttpClientModule } from '@angular/common/http';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,11 @@ import { ToastrModule } from 'ngx-toastr';
     AppRoutingModule,
     ToastrModule.forRoot( {timeOut:10000,positionClass:'toast-top-right',preventDuplicates:true}),
     NgxPaginationModule,
-    Ng2SearchPipeModule  
+    Ng2SearchPipeModule,
+    HttpClientModule,
+    BrowserModule,
+    BrowserAnimationsModule,  
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/p-pharmasist/p-pharmasist-routing.module.ts
+++ src/app/p-pharmasist/p-pharmasist-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MedicineListComponent } from './pharmasist/medicine-list/medicine-list.component';
+import { PatientVMListComponent } from './pharmasist/patient-vm-list/patient-vm-list.component';
+import { HomeComponent } from './pharmasist/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {path:'home',component:HomeComponent},
+  {path:'medicine-list',component:MedicineListComponent},
+  {path:'patient-list',component:PatientVMListComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
