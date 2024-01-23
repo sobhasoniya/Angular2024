6eff98cb9f48e0806fe45ef4513d30b7a2f0f6ea
HimaSBIju <himasbiju@gmail.com>
2024-01-23 17:48:57 +0530

    DoctorModule1

--- src/app/shared/d-patient-history-vm.service.ts
+++ src/app/shared/d-patient-history-vm.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { DPatientHistoryVM } from './d-patient-history-vm';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DPatientHistoryVMService {
+historyList:DPatientHistoryVM[]; //For patient history list
+  constructor(private httpClient:HttpClient) { }
 
-  constructor() { }
+  PatientHistory(patientId: number):Observable<DPatientHistoryVM[]>
+  {
+return this.httpClient.get<DPatientHistoryVM[]>(environment.apiUrl+"api/Doctor/"+patientId);
+  }
 }
